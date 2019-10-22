b98ea95c2e537d4f22b994600e22500591e739be
billypon <billypon@gmail.com>
2019-10-22 11:36:43 +0800

    EventHandler -> autobind

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import 'reflect-metadata';
 
-const eventHandler = Symbol('eventHandler');
+const autobindSymbol = Symbol('autobind');
 
-export function EventHandler(target: any, propertyKey: string): void {
-  let properties: string[] = Reflect.getMetadata(eventHandler, target);
+export function autobind(target: any, propertyKey: string): void {
+  let properties: string[] = Reflect.getMetadata(autobindSymbol, target);
   if (!properties) {
     properties = [ ];
-    Reflect.defineMetadata(eventHandler, properties, target);
+    Reflect.defineMetadata(autobindSymbol, properties, target);
     const constructor = target.constructor.__proto__;
     target.constructor.__proto__ = function () {
       constructor.apply(this, arguments);
