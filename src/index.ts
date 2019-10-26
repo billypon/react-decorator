33beefcff51f344dfc905cf22c44a9f99c7767d2
billypon <billypon@gmail.com>
2019-10-27 01:38:57 +0800

    support bind arguments

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,18 @@ import 'reflect-metadata';
 
 const autobindSymbol = Symbol('autobind');
 
-export function autobind(target: any, propertyKey: string): void {
-  let properties: string[] = Reflect.getMetadata(autobindSymbol, target);
-  if (!properties) {
-    properties = [ ];
-    Reflect.defineMetadata(autobindSymbol, properties, target);
-    const constructor = target.constructor.__proto__;
-    target.constructor.__proto__ = function () {
-      constructor.apply(this, arguments);
-      properties.forEach((x: string) => this[x] = this[x].bind(this));
+export function autobind(...params: any[]) {
+  return (target: any, propertyKey: string) => {
+    let properties: string[] = Reflect.getMetadata(autobindSymbol, target);
+    if (!properties) {
+      properties = [ ];
+      Reflect.defineMetadata(autobindSymbol, properties, target);
+      const constructor = target.constructor.__proto__;
+      target.constructor.__proto__ = function () {
+        constructor.apply(this, arguments);
+        properties.forEach((x: string) => this[x] = this[x].bind(this, ...params));
+      }
     }
+    properties.push(propertyKey);
   }
-  properties.push(propertyKey);
 }
