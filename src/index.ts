804918db36bf25fe796aecdb5baecfe6289d5b0b
billypon <billypon@gmail.com>
2021-02-09 09:49:57 +0800

    refactor code

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,36 @@ import 'reflect-metadata';
 
 const symbol = Symbol('autobind');
 
+function getProps(target: object, initial?: Property[]) {
+  let props: Property[] = Reflect.getMetadata(symbol, target);
+  if (!props) {
+    props = initial;
+    if (props) {
+      Reflect.defineMetadata(symbol, props, target);
+    }
+  }
+  return props
+}
+
+function bindProps(context: object, props: Property[]) {
+  if (props) {
+    props.forEach(({ name, params }) => context[name] = context[name].bind(context, ...params));
+  }
+}
+
+export function markToBind(...params: unknown[]) {
+  return ({ constructor: { prototype } }: any, property: string) => {
+    const props = getProps(prototype, [ ]);
+    props.push({ name: property, params });
+  }
+}
+
+export function startBind(instance: object) {
+  const props: Property[] = getProps((instance as any).__proto__);
+  bindProps(instance, props)
+}
+
 interface Property {
   name: string
-  params: any[]
-}
-
-export function markToBind(...params: any[]) {
-  return ({ constructor: { prototype } }: any, propertyKey: string) => {
-    let properties: Property[] = Reflect.getMetadata(symbol, prototype);
-    if (!properties) {
-      properties = [ ];
-      Reflect.defineMetadata(symbol, properties, prototype);
-    }
-    properties.push({ name: propertyKey, params });
-  }
-}
-
-export function startBind(instance: any) {
-  const properties: Property[] = Reflect.getMetadata(symbol, instance.__proto__);
-  if (properties) {
-    properties.forEach(({ name, params }) => instance[name] = instance[name].bind(instance, ...params));
-  }
+  params: unknown[]
 }
