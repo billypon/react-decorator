b3186460d8fa6a68cbc57c8c69f3392b0129bad3
billypon <billypon@gmail.com>
2021-02-09 09:56:51 +0800

    add bind decorator

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import 'reflect-metadata';
 
 const symbol = Symbol('autobind');
 
-function getProps(target: object, initial?: Property[]) {
+function getProps(target: object, initial?: Property[], callback?: () => void) {
   let props: Property[] = Reflect.getMetadata(symbol, target);
   if (!props) {
     props = initial;
     if (props) {
       Reflect.defineMetadata(symbol, props, target);
+    }
+    if (callback) {
+      callback();
     }
   }
   return props
@@ -16,6 +19,19 @@ function getProps(target: object, initial?: Property[]) {
 function bindProps(context: object, props: Property[]) {
   if (props) {
     props.forEach(({ name, params }) => context[name] = context[name].bind(context, ...params));
+  }
+}
+
+export function bind(...params: unknown[]) {
+  return (target: any, property: string) => {
+    const props = getProps(target, [ ], () => {
+      const constructor = target.constructor.__proto__;
+      target.constructor.__proto__ = function () {
+        constructor.apply(this, arguments);
+        bindProps(this, props)
+      }
+    });
+    props.push({ name: property, params });
   }
 }
 
